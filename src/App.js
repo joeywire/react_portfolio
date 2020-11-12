79d4d32d8cf3e6b7eb93a7d68703146520b5a58f
joeywire <61367542+joeywire@users.noreply.github.com>
2020-11-12 10:49:45 -0600

    AboutSection created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
-import './App.css';
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className="App">
-      <h1>Portfolio React App</h1>
+      <h1>Film</h1>
+      <AboutUs />
     </div>
   );
 }
