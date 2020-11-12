75cdebe398021820e7694cd615c06b438f176d3f
joeywire <61367542+joeywire@users.noreply.github.com>
2020-11-12 13:36:25 -0600

    sytled services section

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,13 +1,14 @@
 import styled from 'styled-components';
 
+import { Layout, Description, Image, Hide } from '../styles';
 import home1 from '../img/home1.png'
 
 
 const AboutSection = () => {
     return (
-        <AboutSC className="about">
-            <DescriptionSC>
-                <div className="title">
+        <Layout >
+            <Description>
+                <div className="title"s>
                     <Hide>
                         <h2>We work to make</h2>
                     </Hide>
@@ -20,44 +21,14 @@ const AboutSection = () => {
                 </div>
                 <p>Contact us for any web development ideas you have - lets taco bout it!</p>
                 <button>Contact Us</button>
-            </DescriptionSC>
-            <ImageSC>
+            </Description>
+            <Image>
                 <img src={home1} alt="Guy with camera"/>
-            </ImageSC>
-        </AboutSC>
+            </Image>
+        </Layout>
     );
 };
 
-//Styled Component 
-const AboutSC = styled.div`
-    min-height: 90vh;
-    display: flex; 
-    align-items: center;
-    justify-content: space-between;
-    padding: 5rem 10rem;
-    color: white;
-`;
 
-const DescriptionSC = styled.div`
-    flex: 1; 
-    padding-right: 5rem;
-    h2{
-        font-weight: lighter;
-    }
-`;
-
-const ImageSC = styled.div`
-    flex: 1;
-    overflow: hidden;
-    img {
-        width: 100%;
-        height: 80vh;
-        object-fit: cover;
-    }
-`;
-
-const Hide = styled.div`
-    overflow: hidden;
-`;
 
 export default AboutSection;
