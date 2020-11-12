630e6302bd99858b48ccde312d3c4df2ec77a0b8
joeywire <61367542+joeywire@users.noreply.github.com>
2020-11-12 12:58:00 -0600

    Styled AboutSection component & created GlobalStyles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import AboutUs from './pages/AboutUs';
+//Global Styles - can i seperate one for reset? 
+import GlobalStyle from './components/GlobalStyle'
 
 
 function App() {
   return (
     <div className="App">
       <h1>Film</h1>
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,27 +1,63 @@
+import styled from 'styled-components';
+
 import home1 from '../img/home1.png'
+
 
 const AboutSection = () => {
     return (
-        <div>
-            <div className="description">
+        <AboutSC className="about">
+            <DescriptionSC>
                 <div className="title">
-                    <div className="hide">
+                    <Hide>
                         <h2>We work to make</h2>
-                    </div>
-                    <div className="hide">
-                        <h2>your <span>dreams</span>come</h2>
-                    </div>
-                    <div className="hide">
+                    </Hide>
+                    <Hide>
+                        <h2>your <span>dreams</span> come</h2>
+                    </Hide>
+                    <Hide>
                         <h2>true.</h2>
-                    </div>
+                    </Hide>
                 </div>
                 <p>Contact us for any web development ideas you have - lets taco bout it!</p>
-            </div>
-            <div className="image">
+                <button>Contact Us</button>
+            </DescriptionSC>
+            <ImageSC>
                 <img src={home1} alt="Guy with camera"/>
-            </div>
-        </div>
-    )
-}
+            </ImageSC>
+        </AboutSC>
+    );
+};
+
+//Styled Component 
+const AboutSC = styled.div`
+    min-height: 90vh;
+    display: flex; 
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: white;
+`;
+
+const DescriptionSC = styled.div`
+    flex: 1; 
+    padding-right: 5rem;
+    h2{
+        font-weight: lighter;
+    }
+`;
+
+const ImageSC = styled.div`
+    flex: 1;
+    overflow: hidden;
+    img {
+        width: 100%;
+        height: 80vh;
+        object-fit: cover;
+    }
+`;
+
+const Hide = styled.div`
+    overflow: hidden;
+`;
 
 export default AboutSection;
