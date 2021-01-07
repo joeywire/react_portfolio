fe74e1c3154c8bee50113ff605b5cf3921789b08
joeywire <61367542+joeywire@users.noreply.github.com>
2021-01-07 14:50:43 -0600

    added transitions to about us page

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,20 +1,18 @@
-
 import { Layout, Description, Image, Hide } from '../styles';
-import home1 from '../img/home1.png'
-//Framer Motion 
-import { motion } from 'framer-motion'; 
+import home1 from '../img/home1.png';
+//Framer Motion
+import { motion } from 'framer-motion';
+import { titleAnim, fade, photoAnim } from '../animation';
 
 const AboutSection = () => {
+  //Framer Motion Variant - Avoid the cess pool of props
 
-    //Framer Motion Variant - Avoid the cess pool of props 
-
-    
-    return (
-        <Layout >
-            <Description>
-                <motion.div>
-                    <Hide>
-                        {/* 
+  return (
+    <Layout>
+      <Description>
+        <motion.div>
+          <Hide>
+            {/* 
                             Framer Motion Notes: Once this is polished lets move these notes elsewhere
 
                             effect takes effect the moment the component renders 
@@ -23,27 +21,27 @@ const AboutSection = () => {
                             With variatn you pass it in as a variable (see where it is defined above) 
                                 - then pass down value for inital and animate as strings (keys we're referencing)
                         */}
-                        <motion.h2>We work to make</motion.h2>
-                    </Hide>
-                    <Hide>
-                        <motion.h2>
-                            your <span>dreams</span> come
-                        </motion.h2>
-                    </Hide>
-                    <Hide>
-                        <motion.h2>true.</motion.h2>
-                    </Hide>
-                </motion.div>
-                <p>Contact us for any web development ideas you have - lets taco bout it!</p>
-                <button>Contact Us</button>
-            </Description>
-            <Image>
-                <img src={home1} alt="Guy with camera"/>
-            </Image>
-        </Layout>
-    );
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
+          </Hide>
+          <Hide>
+            <motion.h2 variants={titleAnim}>
+              your <span>dreams</span> come
+            </motion.h2>
+          </Hide>
+          <Hide>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
+          </Hide>
+        </motion.div>
+        <motion.p variants={fade}>
+          Contact us for any web development ideas you have - lets taco bout it!
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
+      </Description>
+      <Image>
+        <motion.img variants={photoAnim} src={home1} alt="Guy with camera" />
+      </Image>
+    </Layout>
+  );
 };
-
-
 
 export default AboutSection;
